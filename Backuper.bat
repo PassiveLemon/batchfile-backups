cac51b9560ad713a1b66d8c1ec5e91bc09000ce0
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2022-09-28 19:38:16 -0400

    Add files via upload

---
+++ Backuper.bat
@@ -0,0 +1,4 @@
+@echo "|| Backing up data... ||"
+for %%f in (item-*.*) do call %%f
+@echo "|| Data backed up. ||"
+exit
---
+++ item-GameExample.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set item=Example
+set dest=D:\BACKUPS
+set srce=C:\GameExample\save
+
+echo "|| Backing up %item%... ||"
+if not exist "%dest%\%item%\" (
+	echo "|| Making directory for %item%... ||"
+	mkdir "%dest%\%item%\"
+	echo "|| Directory made. ||"
+)
+echo "|| Archiving... ||"
+tar -cf "%dest%\%item%\%item%.tar" "%srce%"
+
+if exist "%dest%\%item%\%item%3.tar" (
+	echo "|| Removing oldest backup ||"
+	del /Q "%dest%\%item%\%item%3.tar"
+)
+if exist "%dest%\%item%\%item%2.tar" (
+	echo "|| Move 2 to 3 ||"
+	ren "%dest%\%item%\%item%2.tar" "%item%3.tar"
+)
+if exist "%dest%\%item%\%item%1.tar" (
+	echo "|| Move 1 to 2 ||"
+	ren "%dest%\%item%\%item%1.tar" "%item%2.tar"
+)
+if exist "%dest%\%item%\%item%.tar" (
+	echo "|| Move new backup to 1 ||"
+	ren "%dest%\%item%\%item%.tar" "%item%1.tar"
+)
+if exist "%dest%\%item%\%item%.tar" (
+	echo "|| Removing %item%.tar. This shouldn't happen. Check your entries. ||"
+	del /Q "%dest%\%item%\%item%.tar"
+)
+echo "|| Successfully backed up %item% ||"
