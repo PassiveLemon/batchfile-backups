022714dca5ea4c361f808918736e4c8e9b21a22b
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2022-10-11 21:29:09 -0400

    Update Backuper.bat

--- Backuper.bat
+++ Backuper.bat
@@ -1,4 +1,10 @@
-@echo "|| Backing up data... ||"
-for %%f in (item-*.*) do call %%f
-@echo "|| Data backed up. ||"
+@echo off
+echo "|| Backing up... ||"
+>logfile.txt 2>&1 (
+	@echo "%DATE% - %TIME%"
+	@echo "|| Backing up data... ||"
+	for %%f in (item-*.*) do call %%f
+	@echo "|| Data backed up. ||"
+)
+echo "|| Backed up. ||"
 exit
