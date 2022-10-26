c7ff44d5e9e5cb4c8f43c44b94cd635da6d67698
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2022-10-26 17:55:23 -0400

    Update Backuper.bat

--- Backuper.bat
+++ Backuper.bat
@@ -7,4 +7,20 @@ echo "|| Backing up... ||"
 	@echo "|| Data backed up. ||"
 )
 echo "|| Backed up. ||"
+if exist "logfile3.txt" (
+	echo "|| Removing oldest backup ||"
+	del /Q "logfile3.txt"
+)
+if exist "logfile2.txt" (
+	echo "|| Move 2 to 3 ||"
+	ren "logfile2.txt" "logfile3.txt"
+)
+if exist "logfile1.txt" (
+	echo "|| Move 1 to 2 ||"
+	ren "logfile1.txt" "logfile2.txt"
+)
+if exist "logfile.txt" (
+	echo "|| Move new backup to 1 ||"
+	ren "logfile.txt" "logfile1.txt"
+)
 exit
