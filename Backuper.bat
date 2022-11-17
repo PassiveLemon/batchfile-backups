576c9567214a935bc7336c233bb4e18f20ee7974
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2022-11-16 22:48:19 -0500

    Add files via upload

--- Backuper.bat
+++ Backuper.bat
@@ -1,26 +1,62 @@
 @echo off
-echo "|| Backing up... ||"
->logfile.txt 2>&1 (
-	@echo "%DATE% - %TIME%"
-	@echo "|| Backing up data... ||"
-	for %%f in (item-*.*) do call %%f
-	@echo "|| Data backed up. ||"
+
+dir /b /s /a "%srce%\" | findstr .>nul || (
+	echo "|| Nothing detected in source directory or directory does not exist for %item%. Skipping... ||"
+	goto skip
 )
-echo "|| Backed up. ||"
-if exist "logfile3.txt" (
+
+if not exist "%dest%\" (
+	echo "|| Making backup directory... ||"
+	mkdir "%dest%\"
+	echo "|| Directory made. ||"
+)
+
+if not exist "%dest%\%item%\" (
+	echo "|| Making directory for %item%... ||"
+	mkdir "%dest%\%item%\"
+	echo "|| Directory made. ||"
+)
+
+echo "|| Making temporary directory and copying to... ||"
+	mkdir "%dest%\%item%temp\"
+	xcopy /Y /s "%srce%\" "%dest%\%item%temp\"
+
+echo "|| Archiving... ||"
+	tar -cf "%dest%\%item%\%item%.tar" "%dest%\%item%temp"
+
+echo "|| Deleting temporary directory... ||"
+	rmdir /s /q "%dest%\%item%temp\"
+
+if exist "%dest%\%item%\%item%3.tar" (
 	echo "|| Removing oldest backup ||"
-	del /Q "logfile3.txt"
+	del /Q "%dest%\%item%\%item%3.tar"
 )
-if exist "logfile2.txt" (
+
+if exist "%dest%\%item%\%item%2.tar" (
 	echo "|| Move 2 to 3 ||"
-	ren "logfile2.txt" "logfile3.txt"
+	ren "%dest%\%item%\%item%2.tar" "%item%3.tar"
 )
-if exist "logfile1.txt" (
+
+if exist "%dest%\%item%\%item%1.tar" (
 	echo "|| Move 1 to 2 ||"
-	ren "logfile1.txt" "logfile2.txt"
+	ren "%dest%\%item%\%item%1.tar" "%item%2.tar"
 )
-if exist "logfile.txt" (
+
+if exist "%dest%\%item%\%item%.tar" (
 	echo "|| Move new backup to 1 ||"
-	ren "logfile.txt" "logfile1.txt"
+	ren "%dest%\%item%\%item%.tar" "%item%1.tar"
 )
-exit
+
+if exist "%dest%\%item%\%item%.tar" (
+	echo "|| Removing %item%.tar. This shouldn't happen. Check your entries. ||"
+	del /Q "%dest%\%item%\%item%.tar"
+)
+
+if exist "%dest%\%item%temp\" (
+	echo "|| Removing %item%temp\. This shouldn't happen. Check your entries. ||"
+	rmdir /s /q "%dest%\%item%temp\"
+)
+
+echo "|| Successfully backed up %item% ||"
+
+:skip
--- Template.bat
+++ Template.bat
@@ -1,36 +1,7 @@
 @echo off
 
 set item=
-set dest=
+set dest=D:\TESTUPS
 set srce=
 
-echo "|| Backing up %item%... ||"
-if not exist "%dest%\%item%\" (
-	echo "|| Making directory for %item%... ||"
-	mkdir "%dest%\%item%\"
-	echo "|| Directory made. ||"
-)
-echo "|| Archiving... ||"
-tar -cf "%dest%\%item%\%item%.tar" "%srce%"
-
-if exist "%dest%\%item%\%item%3.tar" (
-	echo "|| Removing oldest backup ||"
-	del /Q "%dest%\%item%\%item%3.tar"
-)
-if exist "%dest%\%item%\%item%2.tar" (
-	echo "|| Move 2 to 3 ||"
-	ren "%dest%\%item%\%item%2.tar" "%item%3.tar"
-)
-if exist "%dest%\%item%\%item%1.tar" (
-	echo "|| Move 1 to 2 ||"
-	ren "%dest%\%item%\%item%1.tar" "%item%2.tar"
-)
-if exist "%dest%\%item%\%item%.tar" (
-	echo "|| Move new backup to 1 ||"
-	ren "%dest%\%item%\%item%.tar" "%item%1.tar"
-)
-if exist "%dest%\%item%\%item%.tar" (
-	echo "|| Removing %item%.tar. This shouldn't happen. Check your entries. ||"
-	del /Q "%dest%\%item%\%item%.tar"
-)
-echo "|| Successfully backed up %item% ||"
+call Backuper.bat
